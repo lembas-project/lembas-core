827d4a427357b14883ce3c040b4d499880c06e8b
Matt Kramer <mkramer@anaconda.com>
2022-08-05 14:42:16 -0500

    Move run logic to run method

--- examples/planingfsi/run_planingfsi.py
+++ examples/planingfsi/run_planingfsi.py
@@ -22,54 +22,55 @@ class PlaningPlateResults:
     moment: float
 
 
-def run_planingfsi(froude_num: float, angle_of_attack: float) -> PlaningPlateResults:
-    if not (MIN_FROUDE_NUM <= froude_num <= MAX_FROUDE_NUM):
-        raise ValueError("Input Froude number must be in the range 0.2 <= Fr <= 3.0")
-
-    case_dir_base = FLAT_PLATE_ROOT / "flat_plate_base"
-
-    case_dir = FLAT_PLATE_ROOT / f"Fr={froude_num:0.1f}_AOA={angle_of_attack:0.1f}"
-    if not case_dir.exists():
-        shutil.copytree(case_dir_base, case_dir)
-
-        with (case_dir / "configDict").open("w") as fp:
-            fp.write("baseDict: './configDict.base'\n")
-            fp.write(f"Fr: {froude_num}\n")
-            fp.write(f"AOA: {angle_of_attack}\n")
-
-        subprocess.run(["planingfsi", "mesh"], cwd=str(case_dir))
-        subprocess.run(["planingfsi", "run"], cwd=str(case_dir))
-
-    # Find the latest time directory to load results from
-    results_dir = sorted(
-        case_dir.glob("[0-9]*"), key=lambda d: int(d.name), reverse=True
-    )[0]
-    results_dict = load_dict_from_file(results_dir / "forces_total.txt")
-
-    return PlaningPlateResults(
-        drag=results_dict["Drag"],
-        lift=results_dict["Lift"],
-        moment=results_dict["Moment"],
-    )
-
-
 class PlaningPlateCase:
-    def __init__(self, froude_num: float, aoa: float):
+    def __init__(self, froude_num: float, angle_of_attack: float):
         self.froude_num = froude_num
-        self.aoa = aoa
+        self.angle_of_attack = angle_of_attack
 
     def run(self) -> PlaningPlateResults:
-        return run_planingfsi(self.froude_num, self.aoa)
+        if not (MIN_FROUDE_NUM <= self.froude_num <= MAX_FROUDE_NUM):
+            raise ValueError(
+                "Input Froude number must be in the range 0.2 <= Fr <= 3.0"
+            )
+
+        case_dir_base = FLAT_PLATE_ROOT / "flat_plate_base"
+
+        case_dir = (
+            FLAT_PLATE_ROOT
+            / f"Fr={self.froude_num:0.1f}_AOA={self.angle_of_attack:0.1f}"
+        )
+        if not case_dir.exists():
+            shutil.copytree(case_dir_base, case_dir)
+
+            with (case_dir / "configDict").open("w") as fp:
+                fp.write("baseDict: './configDict.base'\n")
+                fp.write(f"Fr: {self.froude_num}\n")
+                fp.write(f"AOA: {self.angle_of_attack}\n")
+
+            subprocess.run(["planingfsi", "mesh"], cwd=str(case_dir))
+            subprocess.run(["planingfsi", "run"], cwd=str(case_dir))
+
+        # Find the latest time directory to load results from
+        results_dir = sorted(
+            case_dir.glob("[0-9]*"), key=lambda d: int(d.name), reverse=True
+        )[0]
+        results_dict = load_dict_from_file(results_dir / "forces_total.txt")
+
+        return PlaningPlateResults(
+            drag=results_dict["Drag"],
+            lift=results_dict["Lift"],
+            moment=results_dict["Moment"],
+        )
 
 
-if __name__ == "__main__":
+def main() -> None:
     froude_nums = numpy.linspace(0.5, 3.0, 7)
     AOA_nums = numpy.linspace(5.0, 15.0, 5)
 
     all_results = []
 
     for froude_num, aoa in itertools.product(froude_nums, AOA_nums):
-        case = PlaningPlateCase(froude_num, aoa)
+        case = PlaningPlateCase(froude_num=froude_num, angle_of_attack=aoa)
         results = case.run()
 
         results_dict = dataclasses.asdict(results)
@@ -80,3 +81,7 @@ if __name__ == "__main__":
     df.plot.scatter(x="froude_num", y="lift", c="aoa")
 
     pyplot.show()
+
+
+if __name__ == "__main__":
+    main()
