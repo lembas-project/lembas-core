06f5a99122d1d433fa919189259ff12598beef98
Matt Kramer <mkramer@anaconda.com>
2022-08-05 20:58:56 -0500

    Add preliminary implementation of InputAttribute

--- examples/planingfsi/run_planingfsi.py
+++ examples/planingfsi/run_planingfsi.py
@@ -31,6 +31,10 @@ class PlaningPlateResults:
 class Case:
     """Base case for all cases."""
 
+    def __init__(self, **kwargs: Any):
+        for name, value in kwargs.items():
+            setattr(self, name, value)
+
 
 def step(condition: Callable[[Any], bool]) -> Any:
     def decorator(f: Callable[[Case], None]) -> Callable[[Case], None]:
@@ -46,10 +50,56 @@ def step(condition: Callable[[Any], bool]) -> Any:
     return decorator
 
 
+class _NoDefault:
+    """Used as a sentinel to indicate lack of a default value for an `InputAttribute`."""
+
+
+_builtin_type = type
+
+
+class InputAttribute:
+    _type: type | None
+    _name: str
+
+    def __init__(self, *, type: type | None = None, default: Any = _NoDefault):
+        if type is not None:
+            self._type = type
+        elif default != _NoDefault:
+            self._type = _builtin_type(default)
+        else:
+            self._type = None
+
+        self._default = default
+
+    def __set_name__(self, owner: type[Case], name: str) -> None:
+        self._name = name
+
+    def __set__(self, instance: object, value: Any) -> None:
+        if self._type is not None:
+            value = self._type(value)
+        instance.__dict__[self._name] = value
+
+    def __get__(self, instance: Case, owner: type[Case]) -> Any:
+        """Retrieve the attribute from the instance dictionary.
+
+        If the value hasn't been set, we attempt to return a default, unless there is no default,
+        in which case we raise an AttributeError.
+
+        """
+        try:
+            return instance.__dict__[self._name]
+        except KeyError:
+            if self._default == _NoDefault:
+                raise AttributeError(
+                    f"'{self._name}' attribute of '{owner.__name__}' class "
+                    "has no default value and must be specified explicitly"
+                )
+            return self._default
+
+
 class PlaningPlateCase(Case):
-    def __init__(self, froude_num: float, angle_of_attack: float):
-        self.froude_num = froude_num
-        self.angle_of_attack = angle_of_attack
+    froude_num = InputAttribute(type=float)
+    angle_of_attack = InputAttribute(type=float)
 
     @property
     def case_dir(self) -> Path:
