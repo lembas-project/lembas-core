3f5c4e69eb69f8d891df7c8a42118738e39791e2
Matt Kramer <mkramer@anaconda.com>
2022-08-09 14:24:41 -0500

    Add CaseList to public API

--- src/lembas/__init__.py
+++ src/lembas/__init__.py
@@ -1,5 +1,6 @@
 from lembas.core import Case
+from lembas.core import CaseList
 from lembas.core import InputParameter
 from lembas.core import step
 
-__all__ = ["Case", "InputParameter", "step"]
+__all__ = ["Case", "CaseList", "InputParameter", "step"]
