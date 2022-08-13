9e3dacd3649409a1aa5966ee23545361ea406915
Matt Kramer <mkramer@anaconda.com>
2022-08-12 22:09:39 -0500

    Add favicon and logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -109,6 +109,8 @@ autodoc_default_options = {
 # a list of builtin themes.
 #
 html_theme = "pydata_sphinx_theme"
+html_logo = "_static/logo.svg"
+html_favicon = "_static/favicon.svg"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
