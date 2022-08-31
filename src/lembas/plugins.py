c1d2e027389acaf56c6885ba3d638d77de104f68
Matt Kramer <mkramer@anaconda.com>
2022-08-31 15:42:29 -0500

    Add some tests for the module generation and loading

---
+++ src/lembas/plugins.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import importlib.util
+import sys
+from pathlib import Path
+from types import ModuleType
+
+
+def _load_module_from_path(mod_path: Path) -> ModuleType:
+    """Load a module from a filesystem Path.
+
+    Args:
+        mod_path: A path to the `.py` file.
+
+    Returns:
+        The imported module object.
+
+    """
+    mod_name = mod_path.stem
+    spec = importlib.util.spec_from_file_location(mod_name, mod_path.as_posix())
+    if spec is None:
+        raise LookupError(f"Cannot load module {mod_path}")
+
+    mod = importlib.util.module_from_spec(spec)
+    sys.modules[mod_name] = mod
+    assert spec.loader is not None
+    spec.loader.exec_module(mod)
+    return mod
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
+from textwrap import dedent
 from typing import TYPE_CHECKING
 from typing import Callable
 
@@ -8,8 +9,10 @@ import pytest
 from typer.testing import CliRunner
 from typer.testing import Result
 
+from lembas import Case
 from lembas import __version__
 from lembas.cli import app
+from lembas.plugins import _load_module_from_path
 
 if TYPE_CHECKING:
     from _pytest.monkeypatch import MonkeyPatch
@@ -41,3 +44,43 @@ def test_version(invoke_cli: CLIInvoker) -> None:
     result = invoke_cli("--version")
     assert result.exit_code == 0
     assert f"Lembas version: {__version__}" in result.stdout
+
+
+@pytest.fixture()
+def plugin_module_path(run_path: Path) -> Path:
+    """Generate a plugin module containing a case handler, returning the path."""
+    code = """\
+        import json
+        from lembas import Case, InputParameter, step
+
+        class MyCase(Case):
+            my_param = InputParameter(type=float, min=2.0, max=5.0)
+            has_been_run = InputParameter(default=False)
+
+            @step
+            def set_has_been_run(self) -> None:
+                self.has_been_run = True
+
+            @step
+            def write_results(self) -> None:
+                with open("results.json", "w") as fp:
+                    json.dump({"has_been_run": self.has_been_run}, fp)
+    """
+    mod_path = run_path / "my_mod.py"
+    with mod_path.open("w") as fp:
+        fp.write(dedent(code))
+    return mod_path
+
+
+def test_plugin_file_exists(plugin_module_path: Path) -> None:
+    """The file exists and is not empty (test fixture itself)."""
+    assert plugin_module_path.exists()
+    with plugin_module_path.open() as fp:
+        assert "MyCase" in fp.read()
+
+
+def test_load_plugin_module(plugin_module_path: Path) -> None:
+    """We can load the module and the class inside it."""
+    module = _load_module_from_path(plugin_module_path)
+    assert hasattr(module, "MyCase")
+    assert issubclass(module.MyCase, Case)
