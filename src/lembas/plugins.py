7ba6a2d43c47e66a65528f0c727be53fa93266c9
Matt Kramer <mkramer@anaconda.com>
2022-08-31 21:56:05 -0500

    Move exception logic to be closer

--- src/lembas/plugins.py
+++ src/lembas/plugins.py
@@ -20,9 +20,10 @@ def _load_module_from_path(mod_path: Path) -> ModuleType:
     spec = importlib.util.spec_from_file_location(mod_name, mod_path)
     if spec is None:
         raise LookupError(f"Cannot load module {mod_path}")
+    if spec.loader is None:
+        raise ValueError(f"Invalid spec loader while loading {mod_path}")
 
     mod = importlib.util.module_from_spec(spec)
     sys.modules[mod_name] = mod
-    assert spec.loader is not None
     spec.loader.exec_module(mod)
     return mod
