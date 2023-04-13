41d433d3fb13830973b3f04c850834b871f0a215
mattkram <mkramer@anaconda.com>
2023-04-13 14:55:34 -0500

    Migrate poetry -> setuptools

--- src/lembas/__init__.py
+++ src/lembas/__init__.py
@@ -2,5 +2,6 @@ from lembas.core import Case
 from lembas.core import CaseList
 from lembas.core import InputParameter
 from lembas.core import step
+from lembas._version import __version__
 
-__all__ = ["Case", "CaseList", "InputParameter", "step"]
+__all__ = ["Case", "CaseList", "InputParameter", "step", "__version__"]
