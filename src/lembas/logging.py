ded490f4c9cd582ddb6ecee699b159ee89ef4259
mattkram <mkramer@anaconda.com>
2023-10-23 23:19:38 -0500

    Change default log level to INFO, allow override with env variable

--- src/lembas/logging.py
+++ src/lembas/logging.py
@@ -1,11 +1,13 @@
 import logging
+import os
 
 from rich.logging import RichHandler
 
 __all__ = ["logger"]
 
+LOG_LEVEL = os.getenv("LEMBAS_LOG_LEVEL", "INFO")
 logging.basicConfig(
-    level="NOTSET",
+    level=LOG_LEVEL,
     format="%(message)s",
     datefmt="[%X]",
     handlers=[RichHandler(show_level=False, show_path=False, show_time=False)],
